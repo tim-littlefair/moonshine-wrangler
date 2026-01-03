c4a4f2bc35f34d6de859d2b494639baeaa4e94ea
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-01-03 18:18:40 +0800

    WIP on finding DSP names in firmware

--- moonshinewrangler/process_classic_firmware.py
+++ moonshinewrangler/process_classic_firmware.py
@@ -13,8 +13,9 @@ ending in roman numerals I to V with or without a 'v2'
 suffix.
 """
 
-import zipfile
 import binascii
+import math
+import zipfile
 
 """ Firmware releases made prior to 2016 were done as files
 with an .upd extension.
@@ -38,10 +39,10 @@ in hex and a hex dump is printed starting a fixed
 number of bytes before the string.
 """
 def find_strings_in_byte_stream(
-    byte_stream, target_strings, bytes_before, buffer_length, string_offsets=[]
+    byte_stream, target_strings, bytes_before, buffer_length, clazz=None
 ):
     retval = {}
-    targets_found = []
+    num_16byte_blocks=math.ceil(buffer_length/16)
     for s in target_strings:
         s_bytes = s.encode("utf-8")
         offset = 0
@@ -58,11 +59,14 @@ def find_strings_in_byte_stream(
             end_offset = offset + buffer_length
             found_line = "\n".join(
                 [f"{offset:08x} {s:20s}"] + 
-                [f"{buffer_hex[n*32:(n+1)*32]}" for n in range(0,7)] +
+                [f"{buffer_hex[n*32:(n+1)*32]}" for n in range(0,num_16byte_blocks)] +
                 [f"{end_offset:08x}"]
             )
             print(found_line)
-            retval[s]=ClassicPreset(buffer)
+            if clazz is not None:
+                retval[s]=clazz(byte_stream,offset)
+            else:
+                retval[s]=buffer
     return retval
 
 def null_terminated_string(byte_string):
@@ -72,14 +76,27 @@ def null_terminated_string(byte_string):
     return str(byte_string,"utf-8")
 
 class ClassicPreset(dict):
-    def __init__(self, byte_stream):
-        self.byte_stream = byte_stream
+    def __init__(self, byte_stream, offset):
+        self.byte_stream = byte_stream[offset:offset+0x7f]
         self["name"]=null_terminated_string(self.byte_stream[0:20])
         self["amp_id"]=self.byte_stream[20]
         self["fx"] = [ self.byte_stream[44+(slot*18)] for slot in range(0,4)]
 
     def effect_id(self,slot):
         return byte_stream[44+(slot*18)]
+    
+class NameList(list):
+    def __init__(self, byte_stream, offset):
+        self.byte_stream = byte_stream[offset:]
+        offset2 = 0
+        while True:
+            next_name = null_terminated_string(self.byte_stream[offset2:])
+            if len(next_name)>0:
+                self.append(next_name)
+                offset2 += len(next_name) + 1
+            else:
+                self.byte_stream = self.byte_stream[0:offset2]
+                break;
     
 if __name__ == "__main__":
     m1v2_upd_bytes = get_upd_stream("_work/reference_files/V2_Mustang1_2.2.zip")
@@ -92,17 +109,22 @@ if __name__ == "__main__":
             "American90sChorus", 
             "Pigs Can Fly", 
             "Chimey Deluxe"
-        ], 0, 0x78
+        ], 0, 0x78, ClassicPreset
     )
     for k in presets.keys():
         print(presets[k])
 
-    amp_names = find_strings_in_byte_stream(
+    dsp_names = find_strings_in_byte_stream(
         m1v2_upd_bytes, [
-            "Studio Preamp",
-            "'57 Champ",
-            "'57 Deluxe"
-        ], 16, 0x78
-    )    
+            "'59 Bassman",   # id 100
+            "'57 Deluxe",    # id 103
+            "Princeton",     # id 106
+            "'57 Champ",     # id 124
+            "Studio Preamp", # id 241
+            "'57 Twin",      # id 246
+        ], 0, 0x100, None
+    )
+    for k in dsp_names:
+        print(k,dsp_names[k])
 
 
