66cbfa3c4823ad53a5715a57446c1cb140ecfc4b
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-01-05 15:39:31 +0800

    Extracting product XML from FUSE installer

--- moonshinewrangler/process_fuse_installer.py
+++ moonshinewrangler/process_fuse_installer.py
@@ -7,7 +7,9 @@
 # to store in version control.
 
 import os
+import re
 import subprocess
+import urllib.parse
 
 
 def _extract_file_bytes_from_dmg(dmg_path, file_entry_path):
@@ -42,33 +44,43 @@ def extract_fender_fuse_db_xml(fuse_xml_path):
     # root element with tag <FXDataBase>.
     # Inside the root element there are multiple elements with tag <Product>,
     # each of which contains the parameters applicable for a single
-    # product or group of products supported by the application.
-    # We choose to preserve only one <Product> element, the one
-    # associated with name "Mustang V2 I/II".
+    # (amplifier) product or group of products supported by the FUSE application.
+    # We choose to preserve one XML file containing the full data and also 
+    # separate XML files for each product/group.
     line_array_index = 0
-    preserve_lines = False
+    full_xml_file = None
+    product_xml_file = None
+    product_start_regex = re.compile(r'<Product Name="([^"]+)" ID="(\d+)">')
+    os.makedirs(fuse_xml_path, exist_ok=True)
+
     while line_array_index < len(fuse_exe_strings):
         line = fuse_exe_strings[line_array_index]
-        if "</FXDataBase>" in line:
-            fuse_fx_db_lines += [line, '']
-            break
-        elif preserve_lines is True:
-            fuse_fx_db_lines += [line]
-            if '</Product>' in line:
-                preserve_lines = False
         if line.startswith("<FXDataBase "):
+            assert line_array_index>0
             previous_line = fuse_exe_strings[line_array_index-1]
             assert '<?xml version="1.0" encoding="utf-8"?>' in previous_line
-            fuse_fx_db_lines += [previous_line, line]
-        elif '<Product Name="Mustang V2 I/II" ID="13">' in line:
-            fuse_fx_db_lines += [line]
-            preserve_lines = True
+            full_xml_file = open(os.path.join(fuse_xml_path,"all_products.xml"),"wt")
+            print(previous_line, file=full_xml_file)
+            # current line will be output at end of loop
+        elif "</FXDataBase>" in line:
+            print(line, file=full_xml_file)
+            full_xml_file = None
+            break
+        elif '</Product>' in line:
+            print(line,file=product_xml_file)
+            product_xml_file=None
+        elif product_start_regex.search(line):
+            match = product_start_regex.search(line)
+            product_name = match.group(1)
+            # make product_name filename safe
+            product_name = product_name.replace(" ","_").replace("/","+")
+            product_xml_filename = f"product{match.group(2)}-{product_name}.xml"
+            product_xml_file = open(os.path.join(fuse_xml_path,product_xml_filename),"wt")
+            # current line will be output at end of loop
+        for stream in ( full_xml_file, product_xml_file):
+            if stream is not None:
+                print(line,file=stream)
         line_array_index += 1
-    fuse_db_xml = "\n".join(fuse_fx_db_lines)
-    os.makedirs(os.path.dirname(fuse_xml_path), exist_ok=True)
-    open(fuse_xml_path, "wt").write(fuse_db_xml)
-    print(f"Fender FUSE database for Mustang V2 I/II extracted to {fuse_xml_path}")
-
 
 if __name__ == "__main__":
-    extract_fender_fuse_db_xml("_work/fuse_data/mustang_I_II_V2_range.xml")
+    extract_fender_fuse_db_xml("_work/fuse_data")
