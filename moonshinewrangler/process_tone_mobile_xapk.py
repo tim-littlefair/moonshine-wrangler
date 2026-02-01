fa18376a1d153196b941550026128b90f60d3bda
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-02-01 20:31:31 +0800

    Isolating other strings

--- moonshinewrangler/process_tone_mobile_xapk.py
+++ moonshinewrangler/process_tone_mobile_xapk.py
@@ -22,6 +22,7 @@ def find_json_strings(mustang_so_stream, tone_mobile_dir):
         input=mustang_so_stream.read(),
     )
     _JSON_PREFIX_PATTERN = re.compile(r'\{\s*"([^"]+)":\s*"([^"]+)"')
+    other_strings = []
     for s in strings_result.stdout.split(b'^'):
         candidate_string = str(s,"utf-8")
         match = _JSON_PREFIX_PATTERN.match(candidate_string)
@@ -29,13 +30,10 @@ def find_json_strings(mustang_so_stream, tone_mobile_dir):
             fname = f"{tone_mobile_dir}/{match.group(1)}-{match.group(2)}.json"
             print(f"Dumping to {fname}")
             open(fname,"wt").write(candidate_string)
-        elif True:
-            pass
-        # reenable this to search for any new terms which are considered interesting
-        elif "FenderId" in candidate_string or "displayName" in candidate_string:
-            print("-----")
-            print(candidate_string)
-            print("-----")
+        else:
+            other_strings += [candidate_string]
+        other_strings_stream = open(f"{tone_mobile_dir}/other_strings.txt","wt")
+        other_strings_stream.write("\n^^^^^^^^^^^\n".join(other_strings))
 
 if __name__ == "__main__":
     _TONE_MOBILE_XAPK_PATH = "./_work/reference_files/Fender Tone_5.0.2.108713_APKPure.xapk"
