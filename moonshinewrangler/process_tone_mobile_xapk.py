d0a42229b3d3d8a713ce9abd5a0ee6ea0c26df5c
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-02-01 21:08:21 +0800

    Scanning all files in mobile .xapk

--- moonshinewrangler/process_tone_mobile_xapk.py
+++ moonshinewrangler/process_tone_mobile_xapk.py
@@ -8,18 +8,22 @@ import subprocess
 import zipfile
 
 
-def extract_mustang_so(xapk_path, config_apk_path, mustang_so_path):
+def extract_streams_and_paths(xapk_path, config_apk_path):
+    retval = []
     xapk_zf = zipfile.ZipFile(xapk_path)
     config_apk_zf = zipfile.ZipFile(xapk_zf.open(config_apk_path))
-    mustang_so_stream = config_apk_zf.open(mustang_so_path)
-    return mustang_so_stream
+    for zi in config_apk_zf.filelist:
+        print(f"Processing {zi.filename}")
+        stream = config_apk_zf.open(zi.filename)
+        retval += [ (stream, zi.filename), ]
+    return retval
 
-def find_json_strings(mustang_so_stream, tone_mobile_dir):
+def find_json_strings(stream_from_apk, apk_path, tone_mobile_dir):
     os.makedirs(tone_mobile_dir, exist_ok=True)    
     strings_result = subprocess.run(
         '/bin/sh -c "/usr/bin/strings -n 10 --include-all-whitespace --output-separator ^"',
         capture_output=True, text=False, shell=True,
-        input=mustang_so_stream.read(),
+        input=stream_from_apk.read(),
     )
     _JSON_PREFIX_PATTERN = re.compile(r'\{\s*"([^"]+)":\s*"([^"]+)"')
     other_strings = []
@@ -28,26 +32,28 @@ def find_json_strings(mustang_so_stream, tone_mobile_dir):
         match = _JSON_PREFIX_PATTERN.match(candidate_string)
         if match is not None:
             fname = f"{tone_mobile_dir}/{match.group(1)}-{match.group(2)}.json"
-            print(f"Dumping to {fname}")
+            print(f"{fname} found in {apk_path}")
             open(fname,"wt").write(candidate_string)
         elif "\n" not in candidate_string:
             continue
         else:
-            other_strings += [ candidate_string ]
-        other_strings_stream = open(f"{tone_mobile_dir}/other_strings.txt","wt")
+            other_strings += [ candidate_string.strip() ]
+    if len(other_strings)>0:
+        other_strings_fname = f"{tone_mobile_dir}/other_strings-{os.path.basename(apk_path)}.txt"
+        other_strings_stream = open(other_strings_fname,"wt")
         other_strings_stream.write("\n^^^^^^^^^^^\n".join(other_strings))
+        print(f"Wrote {len(other_strings)} other strings to {other_strings_fname}")
 
 if __name__ == "__main__":
     _TONE_MOBILE_XAPK_PATH = "./_work/reference_files/Fender Tone_5.0.2.108713_APKPure.xapk"
     _CONFIG_APK_PATH = "config.arm64_v8a.apk"
-    _MUSTANG_SO_PATH = "lib/arm64-v8a/libmustangdevice.so"
 
-    mustang_so_stream = extract_mustang_so(
+    streams_and_paths = extract_streams_and_paths(
         _TONE_MOBILE_XAPK_PATH,
-        _CONFIG_APK_PATH,
-        _MUSTANG_SO_PATH
+        _CONFIG_APK_PATH
     )
-    find_json_strings(mustang_so_stream, "./_work/tone_mobile_dir")
+    for s,p in streams_and_paths:
+        find_json_strings(s, p, "./_work/tone_mobile_dir")
 
 
 
