96ac2efe1799d9c9005c3fbac4316bc3e3727e84
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-02-02 10:22:49 +0800

    Moved extract_streams_and_paths and find_strings to helpers.py

--- moonshinewrangler/process_tone_mobile_xapk.py
+++ moonshinewrangler/process_tone_mobile_xapk.py
@@ -2,10 +2,7 @@
 
 # process_tone_mobile_xapk.py
 
-import os
-import re
-import subprocess
-import zipfile
+import shutil
 
 from helpers import extract_streams_and_paths
 from helpers import find_strings
@@ -14,14 +11,14 @@ from helpers import find_strings
 if __name__ == "__main__":
     _TONE_MOBILE_XAPK_PATH = "./_work/reference_files/Fender Tone_5.0.2.108713_APKPure.xapk"
     _CONFIG_APK_PATH = "config.arm64_v8a.apk"
+    _TONE_MOBILE_DATA = "./_work/tone_mobile_data"
+
+    shutil.rmtree(_TONE_MOBILE_DATA,ignore_errors=True)
 
     streams_and_paths = extract_streams_and_paths(
         _TONE_MOBILE_XAPK_PATH,
         _CONFIG_APK_PATH
     )
     for s,p in streams_and_paths:
-        find_strings(s, p, "./_work/tone_mobile_dir")
-
-
-
+        find_strings(s, p, _TONE_MOBILE_DATA)
 
