e2e678a6ee14393ba83108706d001565134890a0
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-08-29 18:26:04 +0800

    Extracting names of all FUSE modules

--- moonshinewrangler/build_sqlite3_db.py
+++ moonshinewrangler/build_sqlite3_db.py
@@ -2,6 +2,8 @@
 
 import os
 import time
+import traceback
+from xml.dom import minidom
 
 import sqlite3
 
@@ -14,35 +16,35 @@ _DB_SCHEMA = """
         CREATE TABLE module_types (
             module_type_id INTEGER NOT NULL,
             module_type_name TEXT NOT NULL,
+            aliases TEXT,
             PRIMARY KEY(module_type_id)
         );
         CREATE TABLE app_modules (
             app_module_id INTEGER NOT NULL,
-            app_module_type_id INTEGER NOT NULL,
-            app_id INTEGER NOT NULL,
             app_module_name TEXT NOT NULL,
-            PRIMARY KEY(app_module_id),
-            FOREIGN KEY(app_module_type_id) REFERENCES app_module_types,
+            app_id INTEGER NOT NULL,
+            module_type_id INTEGER NOT NULL,
+            PRIMARY KEY(app_module_id AUTOINCREMENT),
+            FOREIGN KEY(module_type_id) REFERENCES module_types,
             FOREIGN KEY(app_id) REFERENCES apps 
         )
     """
 
 APPS = { 
-    0: 'mw-app-neutral', 
-    1: 'fender-fuse', 
-    2: 'fender-tone-lt', 
-    3: 'fender-tone-mobile' 
+    0: ('mw-app-neutral',), 
+    1: ('fender-fuse',),
+    2: ('fender-tone-lt',), 
+    3: ('fender-tone-mobile',), 
 }
 
 MODULE_TYPES = { 
-    0: 'mw-unnassigned', 
-    1: 'amp', 
-    2: 'stomp', 
-    3: 'mod', 
-    4: 'delay', 
-    5: 'reverb', 
-    6: 'eq', 
-    7: 'utility' 
+    0: ('amp', 'Amplifier'),
+    1: ('stomp', 'Distortion'),
+    2: ('mod', 'Modulation'),
+    3: ('delay', None),
+    4: ('reverb', None),
+    5: ('eq', None),
+    6: ('utility', None),
 }
 
 _DB_DIR = "_work/sqlite3_db"
@@ -63,18 +65,49 @@ def populate_metadata(cxn):
     for k in sorted(APPS.keys()):
         cxn.execute(f"""
             INSERT INTO APPS VALUES (
-                {k}, "{APPS[k]}"
+                {k}, ?
             )
-        """)
+        """, APPS[k])
     for k in sorted(MODULE_TYPES.keys()):
         cxn.execute(f"""
-            INSERT INTO MODULE_TYPES VALUES (
-                {k}, "{MODULE_TYPES[k]}"
+            INSERT INTO module_types VALUES (
+                {k}, ?, ?
             )
-        """)
+        """, MODULE_TYPES[k])
+
+def populate_fuse_product_metadata(cxn, xml_filename):
+    _FUSE_MODULE_TYPES = ( "Amplifier", "Distortion", "Modulation", "Delay", "Reverb")
+    ( _FUSE_APP_ID, ) = [ k for k in APPS.keys() if APPS[k][0] == 'fender-fuse' ]
+    document = minidom.parse(xml_filename)
+    for product_node in document.getElementsByTagName("Product"):
+        product_name = product_node.getAttribute("Name")
+        if product_name != "Mustang I/II":
+            continue
+        for module_type in _FUSE_MODULE_TYPES:
+            print(module_type)
+            ((mtid,),) = cxn.execute("""
+                SELECT module_type_id 
+                FROM MODULE_TYPES
+                WHERE aliases LIKE ?
+                OR module_type_name = ?;
+            """, (module_type, module_type,))
+            print(mtid)
+            (product_module_type_node,) = product_node.getElementsByTagName(module_type)
+            print(product_module_type_node,)
+            for module_node in product_node.getElementsByTagName("Module"):
+                module_name = module_node.getAttribute("Name")
+                cxn.execute("""                    
+                    INSERT INTO app_modules (
+                        app_module_name, app_id, module_type_id
+                    ) VALUES (
+                        -- (SELECT seq+1 FROM sqlite_sequence WHERE NAME='app_modules'),
+                        ?, ?, ?
+                    );
+                """, (module_name, _FUSE_APP_ID, mtid,))
+
 
 def dump(cxn):
-    for tbl in ( "apps", "module_types"):
+    for tbl in ( "apps", "module_types", "app_modules"):
         print(f"{tbl}:")
         for row in cxn.execute(f"SELECT * FROM {tbl};"):
             print(row)
@@ -83,6 +116,12 @@ if __name__ == "__main__":
     if os.path.exists(_DB_DIR) is False:
         os.mkdir(_DB_DIR)
     cxn = create_db_schema()
-    populate_metadata(cxn)
-    dump(cxn)
+    try:
+        populate_metadata(cxn)
+        cxn.commit()
+        populate_fuse_product_metadata(cxn,"_work/fuse_data/all_products.xml")
+        dump(cxn)
+    except:
+        traceback.print_exc()
+        dump(cxn)
 
