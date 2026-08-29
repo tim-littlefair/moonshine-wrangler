f134046656dbda10d349cac6bde3f6cecea6a79f
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-08-29 23:45:56 +0800

    Extracted populate_product_metadata_for_app

--- moonshinewrangler/build_sqlite3_db.py
+++ moonshinewrangler/build_sqlite3_db.py
@@ -102,6 +102,8 @@ def populate_fuse_product_metadata(cxn, xml_filename):
     # The schema allows modules to be tracked per product 
     # (i.e. supported physical amplifier model range), 
     # but only worrying about Mustang I/II/III/IV/V at the moment
+    # TODO: Remove filter when one-to-many arity between app_modules 
+    #       and app_product_modules is fixed
     _PRODUCT_NAME_FILTER = "Mustang I/II"
     _get_root_node = lambda xml_filename: minidom.parse(xml_filename)
     _get_product_nodes = lambda root_node: root_node.getElementsByTagName("Product")
@@ -111,19 +113,32 @@ def populate_fuse_product_metadata(cxn, xml_filename):
     )
     _get_module_nodes = lambda product_node: product_node.getElementsByTagName("Module") 
     _get_node_name = lambda module_node: module_node.getAttribute("Name")
+    populate_product_metadata_for_app(
+        cxn, xml_filename, 
+        _FUSE_MODULE_TYPES, _FUSE_APP_ID, _PRODUCT_NAME_FILTER, 
+        _get_root_node, _get_product_nodes, _get_node_name_and_id, _get_module_nodes, _get_node_name
+    )
+
+def populate_product_metadata_for_app(
+        cxn, xml_filename,
+        # per-app constants 
+        app_module_types, app_id, app_product_name_filter, 
+        # per-app lambdas
+        _get_root_node, _get_product_nodes, _get_node_name_and_id, _get_module_nodes, _get_node_name
+):
     root_node = _get_root_node(xml_filename)
     for product_node in _get_product_nodes(root_node):
         product_name, product_id = _get_node_name_and_id(product_node)
         if (
-            _PRODUCT_NAME_FILTER is not None and 
-            product_name not in _PRODUCT_NAME_FILTER
+            app_product_name_filter is not None and 
+            product_name not in app_product_name_filter
         ):
             continue
         cxn.execute(
             "INSERT INTO app_products values ( ?, ?, ? )",
-            (_FUSE_APP_ID, product_id, product_name,)
+            (app_id, product_id, product_name,)
         )
-        for module_type in _FUSE_MODULE_TYPES:
+        for module_type in app_module_types:
             ((mtid,),) = cxn.execute("""
                 SELECT module_type_id 
                 FROM MODULE_TYPES
@@ -132,13 +147,16 @@ def populate_fuse_product_metadata(cxn, xml_filename):
             """, (f"%{module_type}%", module_type,))
             for module_node in _get_module_nodes(product_node):
                 module_name = _get_node_name(module_node)
+                # TODO: search for existing record to establish
+                #       one-to-many arity between app_modules 
+                #       and app_product_modules
                 cxn.execute("""                    
                     INSERT INTO app_modules (
                         app_module_name, app_id, module_type_id
                     ) VALUES (
                         ?, ?, ?
                     );
-                """, (module_name, _FUSE_APP_ID, mtid,))
+                """, (module_name, app_id, mtid,))
                 # the primary key of app_modules is created by AUTOINCREMENT
                 # so we get it from the table
                 ((app_module_id,),) = cxn.execute("SELECT MAX(app_module_id) FROM app_modules")
@@ -146,7 +164,7 @@ def populate_fuse_product_metadata(cxn, xml_filename):
                     INSERT INTO app_product_modules VALUES ( 
                         ?, ?, ?
                     )
-                """, ( app_module_id, product_id, _FUSE_APP_ID))
+                """, ( app_module_id, product_id, app_id))
 
 def dump(cxn):
     for tbl in ( "apps", "app_products", "module_types", "app_modules", "app_product_modules" ):
@@ -160,7 +178,6 @@ if __name__ == "__main__":
     cxn = create_db_schema()
     try:
         populate_metadata(cxn)
-        cxn.commit()
         populate_fuse_product_metadata(cxn,"_work/fuse_data/all_products.xml")
         dump(cxn)
     except:
