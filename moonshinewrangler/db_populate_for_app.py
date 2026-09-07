9625848be889ae57d830682ade2ffc74a8d64afa
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-09-07 13:01:40 +0800

    All FUSE data processed

--- moonshinewrangler/db_populate_for_app.py
+++ moonshinewrangler/db_populate_for_app.py
@@ -1,5 +1,16 @@
 #! python3
 
+import sys
+
+from sqlite3 import Warning as Sqlite3Warning
+
+_ALIAS_SEPARATOR=","
+
+class AppModuleNameResolutionWarning(Sqlite3Warning):
+    def __init__(self,*param_tuple):
+        print("Failed to resolve app module name",file=sys.stderr)
+        print("params:",param_tuple,file=sys.stderr)
+
 def populate_product_metadata_for_app(
         cxn, xml_filename,
         # per-app constants 
@@ -40,26 +51,64 @@ def populate_product_metadata_for_app(
                     AND app_module_id=?
                 """, ( mtid, app_module_id, )).fetchall()
                 if len(preexisting_records_for_module)==0:
-                    cur = cxn.execute("""                    
+                    cxn.execute("""                    
                         INSERT INTO app_modules (
                              module_type_id, app_module_id, app_module_name, aliases, app_id
                         ) VALUES (
                             ?, ?, ?, ?, ?
                         );
                     """, (mtid, app_module_id, module_name, module_aliases, app_id,))
-                    # the primary key of app_modules is created by AUTOINCREMENT
-                    # so we get it from the cursor
-                    app_module_id = cur.lastrowid
-                    cxn.execute("""
-                        INSERT INTO app_product_modules VALUES ( 
-                            ?, ?, ?, ?
-                        )
-                    """, ( mtid, app_module_id, product_id, app_id))
                 else:
                     assert len(preexisting_records_for_module)==1
                     preexisting_record_for_module = preexisting_records_for_module[0]
-                    print(mtid,app_module_id,module_name, ">", preexisting_record_for_module)
                     assert preexisting_record_for_module[0]==mtid
                     assert preexisting_record_for_module[1]==app_module_id
-                    #assert preexisting_record_for_module[2]==module_name
+                    preexisting_name = preexisting_record_for_module[2]
+                    update_name = None
+                    update_aliases = None
+                    extra_alias_candidate = None
+                    if preexisting_name==module_name:
+                        pass
+                    else:
+                        if module_name in preexisting_name:
+                            extra_alias_candidate = module_name
+                            update_name = preexisting_name
+                        elif preexisting_name in module_name:
+                            extra_alias_candidate = preexisting_name
+                            update_name = module_name
+                        else:
+                            raise AppModuleNameResolutionWarning(mtid,app_module_id,module_name, ">", preexisting_record_for_module)
+                    preexisting_aliases = preexisting_record_for_module[4]
+                    if module_aliases in preexisting_aliases.split(_ALIAS_SEPARATOR):
+                        pass
+                    else:
+                        update_aliases = preexisting_aliases + _ALIAS_SEPARATOR + module_aliases
+                    if extra_alias_candidate is None:
+                        pass
+                    elif update_aliases is not None and extra_alias_candidate not in update_aliases.split(_ALIAS_SEPARATOR):
+                        update_aliases = update_aliases + _ALIAS_SEPARATOR + extra_alias_candidate
+                    else:
+                        pass
+
+                    if update_name is None and update_aliases is None:
+                        pass
+                    else:
+                        if update_name is None:
+                            update_name = preexisting_name
+                        if update_aliases is None:
+                            update_aliases = preexisting_aliases
+                        cxn.execute("""                    
+                            UPDATE app_modules SET 
+                            app_module_name=?,
+                            aliases=?
+                            WHERE app_id=?
+                            AND app_module_id=?
+                            AND module_type_id=?
+                        ;
+                        """, (update_name, update_aliases, app_id, app_module_id, mtid,))
+                cxn.execute("""
+                    INSERT INTO app_product_modules VALUES ( 
+                        ?, ?, ?, ?
+                    )
+                """, ( mtid, app_module_id, product_id, app_id))
 
